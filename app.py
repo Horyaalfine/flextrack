6ae58c414645e6bee55d4887fbffb29958fb35ba
Horyaalfine <horyaal@hotmail.com>
2026-06-29 16:04:59 +0100

    Fix default port to 8080

--- app.py
+++ app.py
@@ -328,4 +328,4 @@ if __name__ == '__main__':
     if DATABASE_URL:
         init_db()
     app.run(debug=os.environ.get('FLASK_DEBUG', '0') == '1',
-            host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+            host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
